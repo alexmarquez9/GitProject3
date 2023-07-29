e891cc123192b878f40ccd9efda34c7accbf8ea5
alexmarquez9 <140654902+alexmarquez9@users.noreply.github.com>
2023-07-29 11:53:32 -0400

    Update Test1.java

--- src/day1/Test1.java
+++ src/day1/Test1.java
@@ -5,4 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("Alex Marquez");
 	}
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}	
 }
